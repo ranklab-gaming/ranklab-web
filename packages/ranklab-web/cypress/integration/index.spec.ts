2d8ff329e76bf572e4014b51eef8dbc39f344215
Matteo Depalo <matteo@depalo.me>
2021-10-27 14:49:50 +0000

    Change ranklab.development to ranklab-development

--- packages/ranklab-web/cypress/integration/index.spec.ts
+++ packages/ranklab-web/cypress/integration/index.spec.ts
@@ -1,6 +1,6 @@
 describe("Navigation", () => {
   it("should navigate to the index page", () => {
-    cy.visit("http://ranklab.development:3000/")
+    cy.visit("http://ranklab-development:3000/")
 
     cy.get("h1").contains("Be the better gamer with Ranklab")
   })
