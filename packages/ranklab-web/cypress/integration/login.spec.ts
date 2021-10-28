b5b3fc72f56dffbb3a04e77954f937e34eebfae9
Matteo Depalo <matteo@depalo.me>
2021-10-28 09:26:54 +0000

    Add back NODE_OPTIONS

--- packages/ranklab-web/cypress/integration/login.spec.ts
+++ packages/ranklab-web/cypress/integration/login.spec.ts
@@ -21,8 +21,7 @@ describe("login", () => {
         const callbackUrl = `http://ranklab-development:3000/api/auth/callback?access_token=${access_token}&scope=openid&id_token=${id_token}&expires_in=${expires_in}&token_type=Bearer&state=${auth0State.state}`
         cy.visit(callbackUrl, {
           onBeforeLoad(win) {
-            win.document.cookie =
-              "state=" + auth0State.state + ";nonce=" + auth0State.nonce
+            win.document.cookie = `state=${auth0State.state};nonce=${auth0State.nonce}`
           },
         })
       })
