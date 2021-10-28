ca4d82c132370c6ad769b2bb5adb7ede1b363e4b
Matteo Depalo <matteo@depalo.me>
2021-10-28 11:24:16 +0000

    Add secrets from process.env to cypress

--- packages/ranklab-web/cypress/plugins/index.js
+++ packages/ranklab-web/cypress/plugins/index.js
@@ -2,4 +2,9 @@ const encrypt = require("cypress-nextjs-auth0/encrypt")
 
 module.exports = (on, config) => {
   on("task", { encrypt })
+
+  config.env.auth0ClientSecret = process.env.AUTH0_CLIENT_SECRET
+  config.env.auth0CookieSecret = process.env.AUTH0_SECRET
+
+  return config
 }
