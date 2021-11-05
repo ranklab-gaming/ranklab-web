eae7db5e3f0d6709574307b0842d8769f4c72a52
Matteo Depalo <matteo@depalo.me>
2021-11-05 14:13:45 +0000

    Remove double visit to dashboard

--- packages/ranklab-web/cypress/support/index.js
+++ packages/ranklab-web/cypress/support/index.js
@@ -7,3 +7,21 @@ beforeEach(() => {
 Cypress.Commands.add("sql", (query) => {
   cy.task("db:query", query)
 })
+
+Cypress.Commands.overwrite("login", (originalFn) => {
+  const originalReturnValue = originalFn()
+  const credentials = {
+    username: Cypress.env("auth0Username"),
+    password: Cypress.env("auth0Password"),
+  }
+
+  cy.getUserTokens(credentials).then((response) => {
+    const { accessToken } = response
+
+    cy.getUserInfo(accessToken).then((user) => {
+      cy.sql(`INSERT INTO users (auth0_id) VALUES ('${user.sub}');`)
+    })
+  })
+
+  return originalReturnValue
+})
