612eace624813b26df4eca5b8580de86547efdbc
Eugenio Depalo <eugeniodepalo@gmail.com>
2021-11-09 17:19:35 +0000

    Remove unnecessary envs

--- packages/ranklab-web/cypress/support/login.js
+++ packages/ranklab-web/cypress/support/login.js
@@ -1,14 +1,16 @@
 Cypress.Commands.add("login", () => {
   cy.visit("/api/auth/login")
-  cy.get("input[name=login]").focus().clear().type("test")
-  cy.get("input[name=password]").focus().clear().type("test")
+  cy.get("input[name=login]").focus().clear().type(Cypress.env("auth0Name"))
+  cy.get("input[name=password]")
+    .focus()
+    .clear()
+    .type(Cypress.env("auth0Password"))
   cy.get("button.login").click()
   cy.get("button.login").click()
-
-  cy.getCookie(Cypress.env("auth0SessionCookieName")).then(({ value }) => {
+  cy.getCookie("appSession").then(({ value }) => {
     cy.task("decrypt", {
       value: value,
-      secret: Cypress.env("auth0CookieSecret"),
+      secret: Cypress.env("auth0Secret"),
     }).then(({ user }) => {
       cy.sql(`INSERT INTO users (auth0_id) VALUES ('${user.sub}');`)
     })
