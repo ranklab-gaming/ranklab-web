ec2cc8533bd936957d9e9c8476bf5ab9979ff71b
Matteo Depalo <matteo@depalo.me>
2021-11-10 10:37:23 +0000

    swc minify false

--- packages/ranklab-web/next.config.js
+++ packages/ranklab-web/next.config.js
@@ -8,7 +8,7 @@ const { withSentryConfig } = require("@sentry/nextjs")
 const moduleExports = {
   // temporary workaround https://github.com/getsentry/sentry-javascript/issues/4103
   outputFileTracing: false,
-  swcMinify: true,
+  swcMinify: false,
 }
 
 const sentryWebpackPluginOptions = {
