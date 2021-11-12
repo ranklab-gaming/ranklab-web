8751d05fc70b6f70ca94a975b16d327833f210ab
Eugenio Depalo <eugeniodepalo@gmail.com>
2021-11-12 14:53:16 +0000

    Bump jose to 4.x (#36)
    * Bump jose to 4.x
    
    * Update decrypt.js
    
    * Update decrypt.js
    
    * Update decrypt.js
    
    * Fix typo
    
    * Update decrypt.js
    
    * Update decrypt.js
    
    * Update decrypt.js
    
    Co-authored-by: Matteo Depalo <matteo@depalo.me>

--- packages/ranklab-web/cypress/plugins/decrypt.js
+++ packages/ranklab-web/cypress/plugins/decrypt.js
@@ -1,18 +1,10 @@
 const hkdf = require("futoin-hkdf")
-const { JWE, JWK, JWKS } = require("jose")
+const { createSecretKey } = require("crypto")
+const { compactDecrypt } = require("jose")
 
-module.exports = function ({ value, secret }) {
-  const keystore = new JWKS.KeyStore()
+module.exports = async function ({ value, secret }) {
   const derivedKey = hkdf(secret, 32, { info: "JWE CEK", hash: "SHA-256" })
-  const key = JWK.asKey(derivedKey)
-
-  keystore.add(key)
-
-  return JSON.parse(
-    JWE.decrypt(value, keystore, {
-      complete: true,
-      contentEncryptionAlgorithms: ["A256GCM"],
-      keyManagementAlgorithms: ["dir"],
-    }).cleartext.toString()
-  )
+  const secretKey = createSecretKey(derivedKey)
+  const { plaintext } = await compactDecrypt(value, secretKey)
+  return JSON.parse(new TextDecoder().decode(plaintext))
 }
