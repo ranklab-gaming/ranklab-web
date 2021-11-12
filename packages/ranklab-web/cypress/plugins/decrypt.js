d687741749751a254e6cf7ef471cc39ab827f9f5
Eugenio Depalo <eugeniodepalo@gmail.com>
2021-11-12 14:45:20 +0000

    Update decrypt.js

--- packages/ranklab-web/cypress/plugins/decrypt.js
+++ packages/ranklab-web/cypress/plugins/decrypt.js
@@ -1,17 +1,10 @@
 const hkdf = require("futoin-hkdf")
 const { createSecretKey } = require("crypto")
-const { generalDecrypt, GeneralEncrypt } = require("jose")
+const { compactDecrypt } = require("jose")
 
 module.exports = async function ({ value, secret }) {
   const derivedKey = hkdf(secret, 32, { info: "JWE CEK", hash: "SHA-256" })
   const secretKey = createSecretKey(derivedKey)
-
-  const jwe = await new GeneralEncrypt(new TextEncoder().encode(secretKey))
-    .setProtectedHeader({ enc: "A256GCM" })
-    .addRecipient(secretKey)
-    .setUnprotectedHeader({ alg: "dir" })
-    .encrypt()
-  
-  const { plaintext } = await generalDecrypt(jwe, secretKey)
+  const { plaintext } = await compactDecrypt(value, secretKey)
   return JSON.parse(new TextDecoder().decode(plaintext))
 }
