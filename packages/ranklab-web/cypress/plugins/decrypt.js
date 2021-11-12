03e418bcf21fd8906331b34b50afe8a6213614e8
Eugenio Depalo <eugeniodepalo@gmail.com>
2021-11-12 10:09:45 +0000

    Bump jose to 4.x

--- packages/ranklab-web/cypress/plugins/decrypt.js
+++ packages/ranklab-web/cypress/plugins/decrypt.js
@@ -1,18 +1,10 @@
 const hkdf = require("futoin-hkdf")
-const { JWE, JWK, JWKS } = require("jose")
+const { TextDecoder, generalDecrypt } = require("jose")
 
 module.exports = function ({ value, secret }) {
-  const keystore = new JWKS.KeyStore()
-  const derivedKey = hkdf(secret, 32, { info: "JWE CEK", hash: "SHA-256" })
-  const key = JWK.asKey(derivedKey)
-
-  keystore.add(key)
-
-  return JSON.parse(
-    JWE.decrypt(value, keystore, {
-      complete: true,
-      contentEncryptionAlgorithms: ["A256GCM"],
-      keyManagementAlgorithms: ["dir"],
-    }).cleartext.toString()
-  )
+  const key = hkdf(secret, 32, { info: "JWE CEK", hash: "SHA-256" })
+  const jwe = { ciphertext: value }
+  const decoder = new TextDecoder()
+  const { plaintext } = await generalDecrypt(jwe, key)
+  return JSON.parse(decoder.decode(plaintext))
 }
