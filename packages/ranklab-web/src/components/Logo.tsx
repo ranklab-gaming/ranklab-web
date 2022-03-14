455ef1f1965b56303c9f7e6b847217e9fe3b3181
Eugenio Depalo <eugeniodepalo@gmail.com>
2022-03-14 09:00:14 +0000

    Update logo

--- packages/ranklab-web/src/components/Logo.tsx
+++ packages/ranklab-web/src/components/Logo.tsx
@@ -1,19 +1,14 @@
 import React from "react"
 // material
-import { useTheme } from "@mui/material/styles"
 import { Box, BoxProps } from "@mui/material"
-import flaskOutlineIcon from "@iconify/icons-mdi/flask-outline"
-import { Icon } from "@iconify/react"
+import LogoImage from "../assets/logo"
 
 // ----------------------------------------------------------------------
 
 const Logo = React.forwardRef<any, BoxProps>(({ sx }, ref) => {
-  const theme = useTheme()
-  const PRIMARY_MAIN = theme.palette.primary.main
-
   return (
-    <Box ref={ref} sx={{ cursor: "pointer", ...sx }}>
-      <Icon icon={flaskOutlineIcon} color={PRIMARY_MAIN} fontSize="40px" />
+    <Box ref={ref} sx={{ cursor: "pointer", width: "40px", ...sx }}>
+      <LogoImage />
     </Box>
   )
 })
