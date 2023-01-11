8e53aa6bacce4507407743490ae3402af5da0703
Eugenio Depalo <eugeniodepalo@gmail.com>
2023-01-11 09:32:23 +0000

    Add save payment method checkbox

--- apps/ranklab-web/src/components/ReviewPayment.tsx
+++ apps/ranklab-web/src/components/ReviewPayment.tsx
@@ -1,7 +1,20 @@
 import { LoadingButton } from "@mui/lab"
-import { FormControl, InputLabel, Select, MenuItem } from "@mui/material"
+import {
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  FormControlLabel,
+  Checkbox,
+  useTheme,
+} from "@mui/material"
 import { PaymentMethod } from "@ranklab/api"
-import { PaymentElement, useElements, useStripe } from "@stripe/react-stripe-js"
+import {
+  CardElement,
+  PaymentElement,
+  useElements,
+  useStripe,
+} from "@stripe/react-stripe-js"
 import { FunctionComponent, useState } from "react"
 import * as Yup from "yup"
 import { Controller, useForm } from "react-hook-form"
@@ -9,6 +22,7 @@ import { yupResolver } from "@hookform/resolvers/yup"
 import { capitalize } from "@mui/material"
 import { useRouter } from "next/router"
 import { useSnackbar } from "notistack"
+import { PaymentIntentResult } from "@stripe/stripe-js"
 
 const EMPTY_PAYMENT_METHOD = "EMPTY_PAYMENT_METHOD"
 
@@ -19,10 +33,12 @@ interface ReviewPaymentProps {
 
 type FormValuesProps = {
   paymentMethodId?: string
+  setupFutureUsage: boolean
 }
 
 const FormSchema: Yup.SchemaOf<FormValuesProps> = Yup.object().shape({
   paymentMethodId: Yup.string().optional(),
+  setupFutureUsage: Yup.boolean().required(),
 })
 
 const ReviewPayment: FunctionComponent<ReviewPaymentProps> = ({
@@ -31,6 +47,7 @@ const ReviewPayment: FunctionComponent<ReviewPaymentProps> = ({
 }) => {
   const defaultValues = {
     paymentMethodId: paymentMethods[0]?.id || EMPTY_PAYMENT_METHOD,
+    setupFutureUsage: false,
   }
 
   const { control, handleSubmit, watch } = useForm<FormValuesProps>({
@@ -44,6 +61,7 @@ const ReviewPayment: FunctionComponent<ReviewPaymentProps> = ({
   const [isPaying, setIsPaying] = useState(false)
   const router = useRouter()
   const { enqueueSnackbar } = useSnackbar()
+  const theme = useTheme()
 
   const onSubmit = async (data: FormValuesProps) => {
     setIsPaying(true)
@@ -52,39 +70,76 @@ const ReviewPayment: FunctionComponent<ReviewPaymentProps> = ({
       return
     }
 
+    let result: PaymentIntentResult | undefined
+
     if (data.paymentMethodId !== EMPTY_PAYMENT_METHOD) {
-      const result = await stripe.confirmCardPayment(clientSecret, {
+      result = await stripe.confirmCardPayment(clientSecret, {
         payment_method: data.paymentMethodId,
       })
-
-      if (!result.error) {
-        router.push(
-          "/player/reviews/[id]/success",
-          `/player/reviews/${router.query.id}/success`
-        )
-      } else {
-        enqueueSnackbar(result.error.message!, { variant: "error" })
-        setIsPaying(false)
-      }
     } else {
-      const result = await stripe.confirmPayment({
-        elements,
-        confirmParams: {
-          return_url: `${window.location.href}/success`,
+      result = await stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+          card: elements.getElement(CardElement)!,
         },
+        return_url: `${window.location.href}/success`,
+        setup_future_usage: data.setupFutureUsage ? "off_session" : undefined,
       })
-
-      if (result?.error) {
-        enqueueSnackbar(result.error.message!, { variant: "error" })
-      }
-
-      setIsPaying(false)
     }
+
+    setIsPaying(false)
+
+    if (result.error) {
+      enqueueSnackbar(result.error.message!, { variant: "error" })
+      return
+    }
+
+    router.push(
+      "/player/reviews/[id]/success",
+      `/player/reviews/${router.query.id}/success`
+    )
   }
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      {watch("paymentMethodId") === EMPTY_PAYMENT_METHOD && <PaymentElement />}
+      {watch("paymentMethodId") === EMPTY_PAYMENT_METHOD && (
+        <>
+          <CardElement
+            options={{
+              style: {
+                base: {
+                  fontSize: "16px",
+                  color: theme.palette.text.primary,
+                  "::placeholder": {
+                    color: theme.palette.grey[400],
+                  },
+                },
+                invalid: {
+                  color: theme.palette.error.main,
+                },
+              },
+            }}
+          />
+          <Controller
+            name="setupFutureUsage"
+            control={control}
+            render={({ field }) => (
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={field.value}
+                    onChange={field.onChange}
+                    onBlur={field.onBlur}
+                    sx={{
+                      my: 2,
+                    }}
+                  />
+                }
+                label="Save this card for future purchases"
+              />
+            )}
+          />
+        </>
+      )}
       {paymentMethods.length > 0 && (
         <>
           <Controller
@@ -100,6 +155,7 @@ const ReviewPayment: FunctionComponent<ReviewPaymentProps> = ({
                   onBlur={field.onBlur}
                   error={Boolean(error)}
                   placeholder="Use a different payment method"
+                  sx={{ mb: 2 }}
                 >
                   {paymentMethods.map((paymentMethod) => (
                     <MenuItem key={paymentMethod.id} value={paymentMethod.id}>
