d6c143611e176979894a1b02fab76bb7bfcde1c8
Eugenio Depalo <eugeniodepalo@gmail.com>
2023-04-13 13:30:05 +0000

    Allow all hostnames

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,19 +1,15 @@
 const { withSentryConfig } = require("@sentry/nextjs")
-const url = require("url")
-
-//const parsedUrl = url.parse(process.env.NEXT_PUBLIC_UPLOADS_CDN_URL)
-//const hostname = parsedUrl.hostname
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    // remotePatterns: [
-    //   {
-    //     protocol: "https",
-    //     hostname: hostname,
-    //   },
-    // ],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*",
+      },
+    ],
   },
 }
 
