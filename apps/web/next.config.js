4828289cd259eefc796a9aefe38b7a0eba85be4f
Matteo Depalo <matteo@depalo.me>
2023-04-13 04:48:49 +0000

    Add image remote patterns

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,8 +1,20 @@
 const { withSentryConfig } = require("@sentry/nextjs")
+const url = require("url")
+
+const parsedUrl = url.parse(process.env.NEXT_PUBLIC_UPLOADS_CDN_URL)
+const hostname = parsedUrl.hostname
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: hostname,
+      },
+    ],
+  },
 }
 
 module.exports = withSentryConfig(
