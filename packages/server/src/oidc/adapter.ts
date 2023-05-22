60c2a92890a7d02ee0697b7b6b6ca81d838bee42
Eugenio Depalo <eugeniodepalo@gmail.com>
2023-05-22 16:28:33 +0000

    Log dynamodb errors

--- packages/server/src/oidc/adapter.ts
+++ packages/server/src/oidc/adapter.ts
@@ -21,11 +21,19 @@ const dynamoClient = DynamoDBDocument.from(
   })
   , { marshallOptions: { removeUndefinedValues: true } })
 
+const handleErrors = <T>(fn: () => T) => {
+  try {
+    return fn()
+  } catch (err) {
+    console.error('[dynamodb error]', err)
+    throw err
+  }
+}
+
 export class Adapter implements OidcAdapter {
   name: string
 
   constructor(name: string) {
-    console.log(name)
     this.name = name
   }
 
@@ -34,172 +42,185 @@ export class Adapter implements OidcAdapter {
     payload: AdapterPayload,
     expiresIn?: number
   ): Promise<void> {
-    console.info("[dynamodb] upsert", id, expiresIn)
+    return handleErrors(async () => {
+      const expiresAt = expiresIn
+        ? Math.floor(Date.now() / 1000) + expiresIn
+        : null
 
-    const expiresAt = expiresIn
-      ? Math.floor(Date.now() / 1000) + expiresIn
-      : null
+      const params: UpdateCommandInput = {
+        TableName: TABLE_NAME,
+        Key: { modelId: this.name + "-" + id },
+        UpdateExpression:
+          "SET payload = :payload" +
+          (expiresAt ? ", expiresAt = :expiresAt" : "") +
+          (payload.userCode ? ", userCode = :userCode" : "") +
+          (payload.uid ? ", uid = :uid" : "") +
+          (payload.grantId ? ", grantId = :grantId" : ""),
+        ExpressionAttributeValues: {
+          ":payload": payload,
+          ...(expiresAt ? { ":expiresAt": expiresAt } : {}),
+          ...(payload.userCode ? { ":userCode": payload.userCode } : {}),
+          ...(payload.uid ? { ":uid": payload.uid } : {}),
+          ...(payload.grantId ? { ":grantId": payload.grantId } : {}),
+        },
+      }
 
-    const params: UpdateCommandInput = {
-      TableName: TABLE_NAME,
-      Key: { modelId: this.name + "-" + id },
-      UpdateExpression:
-        "SET payload = :payload" +
-        (expiresAt ? ", expiresAt = :expiresAt" : "") +
-        (payload.userCode ? ", userCode = :userCode" : "") +
-        (payload.uid ? ", uid = :uid" : "") +
-        (payload.grantId ? ", grantId = :grantId" : ""),
-      ExpressionAttributeValues: {
-        ":payload": payload,
-        ...(expiresAt ? { ":expiresAt": expiresAt } : {}),
-        ...(payload.userCode ? { ":userCode": payload.userCode } : {}),
-        ...(payload.uid ? { ":uid": payload.uid } : {}),
-        ...(payload.grantId ? { ":grantId": payload.grantId } : {}),
-      },
-    }
-
-    await dynamoClient.update(params)
+      await dynamoClient.update(params)
+    })
   }
 
   async find(id: string): Promise<AdapterPayload | undefined> {
-    const params: GetCommandInput = {
-      TableName: TABLE_NAME,
-      Key: { modelId: this.name + "-" + id },
-      ProjectionExpression: "payload, expiresAt",
-    }
+    return handleErrors(async () => {
+      const params: GetCommandInput = {
+        TableName: TABLE_NAME,
+        Key: { modelId: this.name + "-" + id },
+        ProjectionExpression: "payload, expiresAt",
+      }
 
-    const result = <
-      { payload: AdapterPayload; expiresAt?: number } | undefined
-      >(await dynamoClient.get(params)).Item
+      const result = <
+        { payload: AdapterPayload; expiresAt?: number } | undefined
+        >(await dynamoClient.get(params)).Item
 
-    if (!result || (result.expiresAt && Date.now() > result.expiresAt * 1000)) {
-      return undefined
-    }
+      if (!result || (result.expiresAt && Date.now() > result.expiresAt * 1000)) {
+        return undefined
+      }
 
-    return result.payload
+      return result.payload
+    })
   }
 
   async findByUserCode(userCode: string): Promise<AdapterPayload | undefined> {
-    const params: QueryCommandInput = {
-      TableName: TABLE_NAME,
-      IndexName: "userCodeIndex",
-      KeyConditionExpression: "userCode = :userCode",
-      ExpressionAttributeValues: {
-        ":userCode": userCode,
-      },
-      Limit: 1,
-      ProjectionExpression: "payload, expiresAt",
-    }
+    return handleErrors(async () => {
+      const params: QueryCommandInput = {
+        TableName: TABLE_NAME,
+        IndexName: "userCodeIndex",
+        KeyConditionExpression: "userCode = :userCode",
+        ExpressionAttributeValues: {
+          ":userCode": userCode,
+        },
+        Limit: 1,
+        ProjectionExpression: "payload, expiresAt",
+      }
 
-    const result = <
-      { payload: AdapterPayload; expiresAt?: number } | undefined
-      >(await dynamoClient.query(params)).Items?.[0]
+      const result = <
+        { payload: AdapterPayload; expiresAt?: number } | undefined
+        >(await dynamoClient.query(params)).Items?.[0]
 
-    if (!result || (result.expiresAt && Date.now() > result.expiresAt * 1000)) {
-      return undefined
-    }
+      if (!result || (result.expiresAt && Date.now() > result.expiresAt * 1000)) {
+        return undefined
+      }
 
-    return result.payload
+      return result.payload
+    })
   }
 
   async findByUid(uid: string): Promise<AdapterPayload | undefined> {
-    const params: QueryCommandInput = {
-      TableName: TABLE_NAME,
-      IndexName: "uidIndex",
-      KeyConditionExpression: "uid = :uid",
-      ExpressionAttributeValues: {
-        ":uid": uid,
-      },
-      Limit: 1,
-      ProjectionExpression: "payload, expiresAt",
-    }
+    return handleErrors(async () => {
+      const params: QueryCommandInput = {
+        TableName: TABLE_NAME,
+        IndexName: "uidIndex",
+        KeyConditionExpression: "uid = :uid",
+        ExpressionAttributeValues: {
+          ":uid": uid,
+        },
+        Limit: 1,
+        ProjectionExpression: "payload, expiresAt",
+      }
 
-    const result = <
-      { payload: AdapterPayload; expiresAt?: number } | undefined
-      >(await dynamoClient.query(params)).Items?.[0]
+      const result = <
+        { payload: AdapterPayload; expiresAt?: number } | undefined
+        >(await dynamoClient.query(params)).Items?.[0]
 
-    if (!result || (result.expiresAt && Date.now() > result.expiresAt * 1000)) {
-      return undefined
-    }
+      if (!result || (result.expiresAt && Date.now() > result.expiresAt * 1000)) {
+        return undefined
+      }
 
-    return result.payload
+      return result.payload
+    })
   }
 
   async consume(id: string): Promise<void> {
-    const params: UpdateCommandInput = {
-      TableName: TABLE_NAME,
-      Key: { modelId: this.name + "-" + id },
-      UpdateExpression: "SET #payload.#consumed = :value",
-      ExpressionAttributeNames: {
-        "#payload": "payload",
-        "#consumed": "consumed",
-      },
-      ExpressionAttributeValues: {
-        ":value": Math.floor(Date.now() / 1000),
-      },
-      ConditionExpression: "attribute_exists(modelId)",
-    }
+    return handleErrors(async () => {
+      const params: UpdateCommandInput = {
+        TableName: TABLE_NAME,
+        Key: { modelId: this.name + "-" + id },
+        UpdateExpression: "SET #payload.#consumed = :value",
+        ExpressionAttributeNames: {
+          "#payload": "payload",
+          "#consumed": "consumed",
+        },
+        ExpressionAttributeValues: {
+          ":value": Math.floor(Date.now() / 1000),
+        },
+        ConditionExpression: "attribute_exists(modelId)",
+      }
 
-    await dynamoClient.update(params)
+      await dynamoClient.update(params)
+    })
   }
 
   async destroy(id: string): Promise<void> {
-    const params: DeleteCommandInput = {
-      TableName: TABLE_NAME,
-      Key: { modelId: this.name + "-" + id },
-    }
+    return handleErrors(async () => {
+      const params: DeleteCommandInput = {
+        TableName: TABLE_NAME,
+        Key: { modelId: this.name + "-" + id },
+      }
 
-    await dynamoClient.delete(params)
+      await dynamoClient.delete(params)
+    })
   }
 
   async revokeByGrantId(grantId: string): Promise<void> {
-    let ExclusiveStartKey: Record<string, AttributeValue> | undefined =
-      undefined
+    return handleErrors(async () => {
+      let ExclusiveStartKey: Record<string, AttributeValue> | undefined =
+        undefined
 
-    do {
-      const params: QueryCommandInput = {
-        TableName: TABLE_NAME,
-        IndexName: "grantIdIndex",
-        KeyConditionExpression: "grantId = :grantId",
-        ExpressionAttributeValues: {
-          ":grantId": grantId,
-        },
-        ProjectionExpression: "modelId",
-        Limit: 25,
-        ExclusiveStartKey,
-      }
+      do {
+        const params: QueryCommandInput = {
+          TableName: TABLE_NAME,
+          IndexName: "grantIdIndex",
+          KeyConditionExpression: "grantId = :grantId",
+          ExpressionAttributeValues: {
+            ":grantId": grantId,
+          },
+          ProjectionExpression: "modelId",
+          Limit: 25,
+          ExclusiveStartKey,
+        }
 
-      const queryResult = await dynamoClient.query(params)
-      ExclusiveStartKey = queryResult.LastEvaluatedKey
+        const queryResult = await dynamoClient.query(params)
+        ExclusiveStartKey = queryResult.LastEvaluatedKey
 
-      const items = <{ modelId: string }[] | undefined>queryResult.Items
+        const items = <{ modelId: string }[] | undefined>queryResult.Items
 
-      if (!items || !items.length) {
-        return
-      }
+        if (!items || !items.length) {
+          return
+        }
 
-      const batchWriteParams: BatchWriteItemInput =
-      {
-        RequestItems: {
-          oidc:
-            items.reduce<WriteRequest[]>(
-              (acc, item) => {
-                const params: DeleteCommandInput = {
-                  TableName: TABLE_NAME,
-                  Key: { modelId: item.modelId },
-                }
+        const batchWriteParams: BatchWriteItemInput =
+        {
+          RequestItems: {
+            oidc:
+              items.reduce<WriteRequest[]>(
+                (acc, item) => {
+                  const params: DeleteCommandInput = {
+                    TableName: TABLE_NAME,
+                    Key: { modelId: item.modelId },
+                  }
 
-                return [
-                  ...acc,
-                  { DeleteRequest: params },
-                ]
-              },
-              []
-            ),
-        },
-      }
+                  return [
+                    ...acc,
+                    { DeleteRequest: params },
+                  ]
+                },
+                []
+              ),
+          },
+        }
 
-      await dynamoClient.batchWrite(batchWriteParams)
-    } while (ExclusiveStartKey)
+        await dynamoClient.batchWrite(batchWriteParams)
+
+      } while (ExclusiveStartKey)
+    })
   }
 }
