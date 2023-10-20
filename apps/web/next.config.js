8e661341e8eddea49a92184df761aad37bf32d2d
Eugenio Depalo <eugeniodepalo@gmail.com>
2023-10-20 17:29:49 +0100

    Filter out chrome extensions

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -3,6 +3,9 @@ const { withSentryConfig } = require("@sentry/nextjs")
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    HOST: process.env.HOST,
+  },
   images: {
     remotePatterns: [
       {
