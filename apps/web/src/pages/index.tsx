e7d8537dde7e2e5e2f73e90b1221c40b1db7d966
Eugenio Depalo <eugeniodepalo@gmail.com>
2023-11-26 10:51:47 +0000

    Update credentials

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,14 +1,12 @@
 import { LandingPage } from "@/components/LandingPage"
-import { GetServerSidePropsContextWithSession } from "@/session"
+import { withSessionSsr } from "@/session"
 import { Game } from "@ranklab/api"
 
 interface Props {
   games: Game[]
 }
 
-export const getServerSideProps = async (
-  ctx: GetServerSidePropsContextWithSession,
-) => {
+export const getServerSideProps = withSessionSsr(async (ctx) => {
   const { createServerApi } = await import("@/api/server")
   const api = await createServerApi(ctx.req)
 
@@ -17,7 +15,7 @@ export const getServerSideProps = async (
       games: await api.gamesList(),
     },
   }
-}
+})
 
 export default function ({ games }: Props) {
   return <LandingPage games={games} />
